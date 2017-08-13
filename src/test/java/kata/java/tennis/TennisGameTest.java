e431d6720a0d05687b2718692565e06675609d79
ibraNdong <30963858+ibraNdong@users.noreply.github.com>
2017-08-13 04:08:02 +0200

    Update TennisGameTest.java

--- src/test/java/kata/java/tennis/TennisGameTest.java
+++ src/test/java/kata/java/tennis/TennisGameTest.java
@@ -1,1 +1,52 @@
+package kata.java.tennis;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TennisGameTest {
+	private Player italia;
+	private Player espania;
+	private Game game;
+	
+	@Before
+	public void setUp(){
+		italia = new Player();
+		espania = new Player();
+		game = new Game(italia, espania);
+	}
+	
+
+
+	@Test
+	public void italiaWinsTheGame_40_15() {
+		italia.score();
+		italia.score();
+		italia.score();
+		
+		espania.score();
+		
+		
+		italia.score();
+		
+		assertThat(game.winner(), is(italia));
+	}
+	
+	@Test
+	public void espaniaWins_15_40() throws Exception {
+		italia.score();
+		
+		espania.score();
+		espania.score();
+		espania.score();
+		
+		
+		espania.score();
+		
+		assertThat(game.winner(), is(espania));
+	}
+	
+	
+}
