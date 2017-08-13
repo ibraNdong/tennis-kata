2e75f24d55086303f5c9d4b11d460994c4c70c34
ibraNdong <30963858+ibraNdong@users.noreply.github.com>
2017-08-13 04:47:09 +0200

    Update TennisPlayerTest.java

--- src/test/java/kata/java/tennis/TennisPlayerTest.java
+++ src/test/java/kata/java/tennis/TennisPlayerTest.java
@@ -1,1 +1,25 @@
 
+package kata.java.tennis;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TennisPlayerTest {
+
+	public TennisPlayer italia;
+
+	@Before
+	public void setUp() {
+		italia = new TennisPlayer();
+	}
+
+	@Test
+	public void aPlayerStartsWithZeroScore() {
+		assertThat(italia.isZero(), is(true));
+	}
+  
+  
+}
