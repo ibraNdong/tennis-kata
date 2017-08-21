232aefac154c5f44a625f88926d72245605dfa5f
ibraNdong <30963858+ibraNdong@users.noreply.github.com>
2017-08-22 01:27:31 +0200

    SPRINT2 : manage a Tennis SET within a tennis match. User Story 1, User Story 2
    SPRINT2 : manage a Tennis SET within a tennis match
    
    User Story 1 :
    
                    As a tennis referee
    
    I want to manage the score of a set of a tennis match between 2 players
    
    In order to display the current Game & Set score of each player
    
     
    
                    Rules details:
    
    -          The set starts with a score of 0 Game for each player
    
    -          Each time a player win a Game, the Set score changes as follow:
    
    1 -> 2 -> 3 -> 4 -> 5 -> 6 (-> 7)
    
    -          If a player reach the Set score of 6 and the other player has a Set score of 4 or lower, the player win the Set
    
    -          If a player win a Game and reach the Set score of 6 and the other player has a Set score of 5, a new Game must be played and the first player who reach the score of 7 wins the match
    
     
    
    User Story 2 :
    
                    As a tennis referee
    
    I want to manage the specific of the rule of Tie-Break at the end of the Set
    
    In order to display the current Game, Set score & Tie-Break score of each player
    
     
    
                    Rules details:
    
    -          If the 2 players reach the score of 6 Games , the Tie-Break rule is activated
    
    -          Each time a player win a point, the score changes as follow:
    
    1 -> 2 -> 3 -> 4 -> 5 -> 6 (-> 7-> 8-> 9-> 10-> …)
    
    -          The Tie-Break ends as soon as a player gets a least 6 points and gets 2 points more than his opponent
    
    -          The player who wins the Tie-Break wins the Set and the match

--- src/java/kata/java/sprint2/ManageSetTennisMatch.java
+++ src/java/kata/java/sprint2/ManageSetTennisMatch.java
@@ -1,1 +1,203 @@
 
+package kata.java.tennis;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+/**
+ * Tennis Score Manager
+ * @author Ibrahima Ndong
+ *
+ */
+public class ManageSetTennisMatch {
+
+        protected Player player1,player2;
+        protected Boolean gameOver;
+        private String playerName;
+        private int set;
+        private int lastSetWinner;
+
+        ManageSetTennisMatch(String playerName1, String playerName2){
+            player1 = new Player(playerName1);
+            player2 = new Player(playerName2);
+            gameOver = Boolean.FALSE;
+            set = 0;
+        }
+
+        /**
+         * Method to play the bout
+         */
+        public void match(){
+            while(true){
+
+                //Get the winner of the bout
+                playerName = getWinner();
+
+                //If player 1 wins the bout
+                if(player1.getName().equalsIgnoreCase(playerName)){
+                    if(player1.isOnMatchPoint()){
+                        System.out.println(player1.getName() + " a gangé le match");
+                        player1.updateSetScore();
+                        displayScore();
+                        break;
+                    }
+                    else{
+                        player1.updateScore();
+                        displayScore();
+                        if(player1.isOnMatchPoint() && player2.isOnMatchPoint()){
+                            tieBreaker(); //Starting tie breaker
+                            break;
+                        }
+                    }
+                }
+                else if(player2.getName().equalsIgnoreCase(playerName)){ //If player 2 wins the bout
+                    if(player2.isOnMatchPoint()){
+                        System.out.println(player1.getName() + " a gagné le match");
+                        player2.updateSetScore();
+                        displayScore();
+                        break;
+                    }
+                    else{
+                        player2.updateScore();
+                        displayScore();
+                        if(player1.isOnMatchPoint() && player2.isOnMatchPoint()){
+                            tieBreaker(); //Starting tie breaker
+                            break;
+                        }
+                    }
+                }
+                else{
+                    System.out.println("Nom incorrect. saisissez encore s'il vous plait.......");
+                }
+            }
+
+            //Reset the score card for next bout
+            player1.resetScore();
+            player2.resetScore();
+        }
+
+        public void deriveWinner(){
+
+            if(player1.isOnSetPoint() || player2.isOnSetPoint()){ //winner is calculated if any of the player is on set point i.e., 6
+                if(player1.getSetPoint() - player2.getSetPoint() >= 2){ // we get a winner when a diff of 2 or more is there between set points of the players
+                    System.out.println(player1.getName() + " est le vainqueur du set " + (set + 1));
+                    if((set == 1 && lastSetWinner == 1) || set == 2){
+                        System.out.println(player1.getName() + " est l'ultimate vainqueur ");
+                        gameOver = Boolean.TRUE;
+                        return;
+                    }
+                    lastSetWinner = 1;
+                    //Starting the next set
+                    set++;
+                    player1.startNextSet();
+                    player2.startNextSet();
+                }
+                else if(player2.getSetPoint() - player1.getSetPoint() >= 2){ // we get a winner when a diff of 2 or more is there between set points of the players
+                    System.out.println(player2.getName() + " est le vainqueur du set " + (set + 1));
+                    if((set == 1 && lastSetWinner == 2) || set == 2){
+                        System.out.println(player2.getName() + " est l'ultimate vainqueur ");
+                        gameOver = Boolean.TRUE;
+                        return;
+                    }
+                    lastSetWinner = 2;
+                    //Starting the next set
+                    set++;
+                    player1.startNextSet();
+                    player2.startNextSet();
+                }
+            }
+        }
+
+    /**
+     * Method to display the current score board
+     */
+    private void displayScore(){
+        System.out.println("Score : ");
+        System.out.println(player1.getName() + " : " + player1.getCurrentScore());
+        System.out.println(player2.getName() + " : " + player2.getCurrentScore());
+
+        System.out.println("Set : ");
+        System.out.println(player1.getName() + " : " + player1.getSetScoreString());
+        System.out.println(player2.getName() + " : " + player2.getSetScoreString());
+    }
+
+    /**
+     * Method to input the current bout winner
+     * @return name of the winner per bout
+     */
+    private String getWinner(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Entrer le nom du joueur qui a gagné le point.");
+        return reader.next().trim();
+    }
+
+    /**
+     * Method to play the tie breaker
+     */
+    private void tieBreaker(){
+        System.out.println("Deuce...");
+
+        while(true){
+            //Get the winner of the bout
+            playerName = getWinner();
+
+            //If player 1 wins the bout
+            if(player1.getName().equalsIgnoreCase(playerName)){
+                if(player1.isOnAdvantage){
+                    player1.setAdvantage(Boolean.FALSE);
+                    if(! player2.isOnAdvantage){
+                        System.out.println(player1.getName() + " a gagné le match ");
+                        player1.updateSetScore();
+                        displayScore();
+                        break;
+                    }
+                }
+                else{
+                    if( ! player2.isOnAdvantage){ //player 1 has the advantage
+                        player1.setAdvantage(Boolean.TRUE);
+                        System.out.println("Advantage ..." + player1.getName());
+                    }
+                    else{ //Both players are returned to deuce
+                        player1.setAdvantage(Boolean.FALSE);
+                        player2.setAdvantage(Boolean.FALSE);
+                        System.out.println("Deuce...");
+                    }
+                }
+            }
+            else if(player2.getName().equalsIgnoreCase(playerName)){ //If player 2 wins the bout
+                if(player2.isOnAdvantage){
+                    player2.setAdvantage(Boolean.FALSE);
+                    if(! player1.isOnAdvantage){
+                        System.out.println(player2.getName() + " a gagné le match");
+                        player2.updateSetScore();
+                        displayScore();
+                        break;
+                    }
+                    else{
+                        player1.setAdvantage(Boolean.FALSE);
+                        System.out.println("Deuce...");
+                    }
+                }
+                else{
+                    if( ! player1.isOnAdvantage){ //player 2 has the advantage
+                        player2.setAdvantage(Boolean.TRUE);
+                        System.out.println("Advantage ..." + player2.getName());
+                    }
+                    else{ //Both players are returned to deuce
+                        player2.setAdvantage(Boolean.FALSE);
+                        player1.setAdvantage(Boolean.FALSE);
+                        System.out.println("Deuce...");
+                    }
+                }
+            }
+            else{
+                System.out.println("Nom incorrect. saisissez encore s'il vous plait.......");
+            }
+        }
+    }
+
+}
+
