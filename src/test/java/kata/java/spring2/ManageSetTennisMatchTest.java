15efa4cd7ab105a1e00b982f3dd17bd4057a7e5b
ibraNdong <30963858+ibraNdong@users.noreply.github.com>
2017-08-22 02:11:05 +0200

    Update ManageSetTennisMatchTest.java

--- src/test/java/kata/java/spring2/ManageSetTennisMatchTest.java
+++ src/test/java/kata/java/spring2/ManageSetTennisMatchTest.java
@@ -3,9 +3,7 @@ package kata.java.sprint2;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.Scanner;
-
 import static org.junit.Assert.*;
 
 public class ManageSetTennisMatchTest {
@@ -13,11 +11,13 @@ public class ManageSetTennisMatchTest {
 
     @Test
     public static void main(String[] args) {
+        
         Scanner reader = new Scanner(System.in);
         System.out.println("Entrer le nom du premier joueur.");
         String playerName1 = reader.next().trim();
         System.out.println("Entrer le nom du second joueur.");
         String playerName2 = reader.next().trim();
+        
         ManageSetTennisMatch scorer = new ManageSetTennisMatch(playerName1, playerName2);
 
         while(! scorer.gameOver){ //run until game is over
